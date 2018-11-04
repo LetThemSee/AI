79702ecd043622024394a60ebb4887ca1355fcd2
LetThemSee <jinzhaochang2017@gmail.com>
2018-11-04 09:54:29 -0500

    upload

---
+++ 8_puzzle/QueueAndStack.py
@@ -0,0 +1,38 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+    
+    def get_contents(self):
+        return self.items
+    
+class Stack: 
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enstack(self, item):
+        self.items.append(item)
+
+    def destack(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+    
+    def get_contents(self):
+        return self.items
+
